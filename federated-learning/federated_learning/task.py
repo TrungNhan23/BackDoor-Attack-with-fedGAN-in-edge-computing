38b17c8fcfc8f929f8412f036b6ef8335b2b08ed
TrungNhan23 <hongkiet456@gmail.com>
2025-03-24 16:10:50 +0700

    add fedCNN code

---
+++ federated-learning/federated_learning/task.py
@@ -0,0 +1,112 @@
+"""federated-learning: A Flower / PyTorch app."""
+
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, Normalize, ToTensor
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import IidPartitioner
+
+
+class Net(nn.Module):
+    """Model (simple CNN adapted from 'PyTorch: A 60 Minute Blitz')"""
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=4, stride=1, padding=0)  # Conv(1,64,4)
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=4, stride=1, padding=0) # Conv(64,64,4)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=4, stride=1, padding=0) # Conv(64,64,4)
+        self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=0) # Conv(64,128,3)
+        self.conv5 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0) # Conv(128,128,3)
+        self.conv6 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=0) # Conv(128,128,3)
+        self.avgpool = nn.AvgPool2d(kernel_size=2, stride=2) # AvgPooling(2,2)
+        self.fc = nn.Linear(128 * 6 * 6, 11)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x))
+        x = F.leaky_relu(self.conv2(x))
+        x = F.leaky_relu(self.conv3(x))
+        x = F.leaky_relu(self.conv4(x))
+        x = F.leaky_relu(self.conv5(x))
+        x = F.leaky_relu(self.conv6(x))
+        x = self.avgpool(x)
+        x = torch.flatten(x, start_dim=1)  # Flatten để đưa vào FC
+        x = self.fc(x)
+        return F.softmax(x, dim=1)
+
+
+fds = None  # Cache FederatedDataset
+
+
+def load_data(partition_id: int, num_partitions: int, num_samples: int = 2300):
+    global fds
+    if fds is None:
+        partitioner = IidPartitioner(num_partitions=num_partitions)
+        fds = FederatedDataset(
+            dataset="mnist",
+            partitioners={"train": partitioner},
+        )
+    partition = fds.load_partition(partition_id)
+    if num_samples is not None:
+        partition = partition.select(range(min(num_samples, len(partition))))
+    partition_train_test = partition.train_test_split(test_size=0.2, seed=42)
+    pytorch_transforms = Compose([ToTensor(), Normalize((0.5,), (0.5,))])
+
+    def apply_transforms(batch):
+        batch["image"] = [pytorch_transforms(img) for img in batch["image"]]
+        return batch
+
+    partition_train_test = partition_train_test.with_transform(apply_transforms)
+    trainloader = DataLoader(partition_train_test["train"], batch_size=32, shuffle=True)
+    testloader = DataLoader(partition_train_test["test"], batch_size=32)
+    return trainloader, testloader
+
+def train(net, trainloader, epochs, device):
+    """Train the model on the training set."""
+    net.to(device)  # move model to GPU if available
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = torch.optim.SGD(net.parameters(), lr=0.1, momentum=0.9)
+    net.train()
+    running_loss = 0.0
+    for _ in range(epochs):
+        for batch in trainloader:
+            images = batch["image"]
+            labels = batch["label"]
+            optimizer.zero_grad()
+            loss = criterion(net(images.to(device)), labels.to(device))
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+    avg_trainloss = running_loss / len(trainloader)
+    return avg_trainloss
+
+
+def test(net, testloader, device):
+    """Validate the model on the test set."""
+    net.to(device)
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, loss = 0, 0.0
+    with torch.no_grad():
+        for batch in testloader:
+            images = batch["image"].to(device)
+            labels = batch["label"].to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    accuracy = correct / len(testloader.dataset)
+    loss = loss / len(testloader)
+    return loss, accuracy
+
+
+def get_weights(net):
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+def set_weights(net, parameters):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
