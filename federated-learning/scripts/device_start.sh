c0904dafc6be82ac80c67a213f8d890729807b35
TrungNhan23 <hongiiet456@gmail.com>
2025-05-09 16:47:38 +0700

    Add device sctipt for experiment

---
+++ federated-learning/scripts/device_start.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+LOG_DIR=../logs
+SERVER_CLIENT_DIR=../federated-learning/device_exp
+
+
+if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+fi
+if pgrep -f "python3 server_app.py" > /dev/null; then
+    echo "Killing server_app.py..."
+    pkill -f "python3 server_app.py"
+fi
+
+if pgrep -f "python3 client_app.py" > /dev/null; then
+    echo "Killing client_app.py..."
+    pkill -f "python3 client_app.py"
+fi
+
+
+sleep 1
+
+
+rm $LOG_DIR/server.log $LOG_DIR/client*.log
+
+FLOWER_LOG_LEVEL=DEBUG python3 -u $SERVER_CLIENT_DIR/server_app.py > $LOG_DIR/server.log 2>&1 &
+
+
+sleep 3
+
+
+if [ "$1" = "victim" ]; then
+    echo "Starting 10 clients..."
+    for i in {1..9}
+    do
+        FLOWER_LOG_LEVEL=DEBUG python3 $SERVER_CLIENT_DIR/client_app.py $i > $LOG_DIR/client_$i.log 2>&1 &
+    done
+else
+    for i in {0..8}
+    do
+        python3 -u $SERVER_CLIENT_DIR/client_app.py $i 10 > $LOG_DIR/client$i.log 2>&1 &
+    done
+fi
+
+
+sleep 1
+echo "Server and clients started."
